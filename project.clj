473e378235156054b47bb3cbeea377ad1136e448
Joe Snikeris <joe@snikeris.com>
2011-07-06 23:37:30 -0400

    hello world implemented

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject blog "1.0.0-SNAPSHOT"
   :description "A blog"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [compojure "0.6.4"]
+                 [enlive "1.0.0"]]
+  :dev-dependencies [[lein-ring "0.4.5"]
+                     [swank-clojure "1.3.0"]]
+  :ring {:handler blog.core/app})
--- src/blog/core.clj
+++ src/blog/core.clj
@@ -1,1 +1,9 @@
-(ns blog.core)
+(ns blog.core
+  (:use [compojure.core :only (defroutes GET POST)])
+  (:require [compojure.route :as route]))
+
+(defroutes routes
+  (GET "/" [] "Hello, world")
+  (route/resources "/"))
+
+(def app routes)
