4ab5b3601941a69c5d02ff3d0f744d8aa47e8fa8
Joe Snikeris <joe@snikeris.com>
2011-07-07 17:30:08 -0400

    use utf-8 charset unless otherwise specified

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject blog "1.0.0-SNAPSHOT"
   :description "A blog"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]
--- src/blog/core.clj
+++ src/blog/core.clj
@@ -1,9 +1,32 @@
 (ns blog.core
-  (:use [compojure.core :only (defroutes GET POST)])
-  (:require [compojure.route :as route]))
+  (:use [compojure.core :only (defroutes GET POST)]
+        [net.cgrand.enlive-html :only (deftemplate defsnippet)])
+  (:require [compojure.route :as route]
+            [net.cgrand.enlive-html :as html]
+            [ring.adapter.jetty :as jetty]))
+
+(deftemplate main "templates/main.html" []
+  [:p] (html/content "Hello, World!"))
 
 (defroutes routes
-  (GET "/" [] "Hello, world")
+  (GET "/" [] (apply str (main)))
   (route/resources "/"))
 
-(def app routes)
+(defn wrap-charset [handler charset]
+  (fn [request]
+    (if-let [response (handler request)]
+      (if-let [content-type (get-in response [:headers "Content-Type"])]
+        (if (.contains content-type "charset")
+          response
+          (assoc-in response
+            [:headers "Content-Type"]
+            (str content-type "; charset=" charset)))
+        response))))
+
+(def app
+  (-> routes
+      (wrap-charset "utf-8")))
+
+;; (defonce *server*
+;;   ;; #' (var) allows rebinding of 'app' to be reflected immediately
+;;   (jetty/run-jetty #'app {:port 8080 :join? false}))
