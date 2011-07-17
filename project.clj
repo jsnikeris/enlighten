9a51c5b131c02988d5d3969203a19e17e0593986
Joe Snikeris <joe@snikeris.com>
2011-07-17 15:18:36 -0400

    happy path of POST implemented

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]
-                 [enlive "1.0.0"]]
+                 [enlive "1.0.0"]
+                 [clj-time "0.3.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]
                      [swank-clojure "1.3.0"]]
   :ring {:handler blog.core/app})
--- src/blog/core.clj
+++ src/blog/core.clj
@@ -1,14 +1,83 @@
+;; Considerations:
+;;   - the edit-url for an entry is also its permalink
+
 (ns blog.core
-  (:use [compojure.core :only (defroutes GET POST)]
-        [net.cgrand.enlive-html :only (deftemplate defsnippet)])
-  (:require [compojure.route :as route]
-            [net.cgrand.enlive-html :as html]
+  (:use [compojure.core :only (defroutes GET POST)])
+  (:require [clojure.string :as str]
+            [clj-time.core :as time]
+            [clj-time.format :as tf]
+            [compojure.route :as route]
+            [net.cgrand.enlive-html :as e]
             [ring.adapter.jetty :as jetty]))
 
-(deftemplate main "templates/main.html" [])
+;; TODO: properties file?
+(def *post-url* "http://localhost:8080/")
+(def *entry-dir* "/home/joe/Documents/Blog/Entries/")
+
+(e/deftemplate main "templates/main.html" []
+  [[:link (e/attr= :rel "service.post")]] (e/set-attr :href *post-url*))
+
+;; TODO: handle other special characters
+(defn hyphenize
+  "Replaces spaces with dashes and lower-cases"
+  [s]
+  (-> s
+      (str/replace " " "-")
+      str/lower-case))
+
+(defn make-edit-url
+  "returns URL of the form: <*post-url*>/2011/apr/this-is-the-title"
+  [date title]
+  (let [month-formatter (tf/formatter "MMM")
+        month (->> date (tf/unparse month-formatter) str/lower-case)
+        [_ base-url] (re-find #"(.*?)/?$" *post-url*)]
+    (str/join "/" [base-url (time/year date) month (hyphenize title)])))
+
+(defn make-tag-uri
+  "returns a tag URI given a DateTime and a URL"
+  [date url]
+  (let [host (.getHost url)
+        path (.getPath url)
+        date-str (tf/unparse (tf/formatters :year-month-day) date)]
+    (str "tag:" host "," date-str ":" path)))
+
+(defn select-text
+  "returns the text of the first matching node"
+  [selector node-or-nodes]
+  (-> node-or-nodes (e/select selector) first e/text))
+
+(defn make-filename [entry]
+  (let [title (select-text [:title] entry)
+        date (->> (select-text [:published] entry)
+                  tf/parse
+                  (tf/unparse (tf/formatters :year-month-day)))]
+    (str date "-" (hyphenize title))))
+
+(defn populate-entry [entry]
+  (let [pub-date (time/now)
+        title (select-text [:title] entry)
+        edit-url (make-edit-url pub-date title)
+        id (make-tag-uri pub-date (java.net.URL. edit-url))]
+    (e/at entry
+          [[:link #{(e/attr= :rel "edit")
+                    (e/attr= :rel "alternate")}]] (e/set-attr :href edit-url)
+          [:id] (e/content id)
+          [#{:published :updated}] (e/content (str pub-date)))))
+
+(defn post-response [entry]
+  (apply str (e/emit* entry)))
+
+;; TODO: validate/normalize Entry
+(defn handle-post [body]
+  (let [entry (populate-entry (e/xml-resource body))
+        filename (make-filename entry)]
+    (spit (str *entry-dir* filename ".xml")
+          (apply str (e/emit* entry)))
+    (post-response entry)))
 
 (defroutes routes
   (GET "/" [] (apply str (main)))
+  (POST *post-url* {body :body} (handle-post body))
   (route/resources "/"))
 
 (defn wrap-charset [handler charset]
