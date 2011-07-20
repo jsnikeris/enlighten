d7060f7e1d5aed6a9a38357747515ffede9204a6
Joe Snikeris <joe@snikeris.com>
2011-07-20 17:44:31 -0400

    added a content-type test for post

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.5"]
                  [enlive "1.0.0"]
+                 [ring-mock "0.1.1"]
                  [clj-time "0.3.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]
                      [swank-clojure "1.3.0"]]
--- test/enlighten/test/core.clj
+++ test/enlighten/test/core.clj
@@ -1,6 +1,13 @@
 (ns enlighten.test.core
-  (:use [enlighten.core])
-  (:use [clojure.test]))
+  (:use [clojure.test]
+        [ring.mock.request]
+        [enlighten.core]
+        [enlighten.atom])
+  (:require [net.cgrand.enlive-html :as e]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest post
+  (let [entry (e/xml-resource "test/post.xml")
+        req (body (request :post (str *post-url*))
+                  (str-entry entry))
+        {:keys [headers] :as resp} (app req)]
+    (is (= (headers "Content-Type") "application/atom+xml; charset=utf-8"))))
