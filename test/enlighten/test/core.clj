79d78fb83f71f7ffd63cdde2cb3ff03fa546ba60
Joe Snikeris <joe@snikeris.com>
2011-07-20 22:57:38 -0400

    added more tests for post

--- test/enlighten/test/core.clj
+++ test/enlighten/test/core.clj
@@ -6,8 +6,16 @@
   (:require [net.cgrand.enlive-html :as e]))
 
 (deftest post
-  (let [entry (e/xml-resource "test/post.xml")
+  (let [req-entry (e/xml-resource "test/post.xml")
+        title (select-text [:title] req-entry)
         req (body (request :post (str *post-url*))
-                  (str-entry entry))
-        {:keys [headers] :as resp} (app req)]
-    (is (= (headers "Content-Type") "application/atom+xml; charset=utf-8"))))
+                  (str-entry req-entry))
+        {:keys [headers body]} (app req)
+        resp-entry (e/html-snippet body)
+        [edit-link] (e/select resp-entry [[:link (e/attr= :rel "edit")]])
+        [alt-link]  (e/select resp-entry [[:link (e/attr= :rel "alternate")]])
+        edit-href (-> edit-link :attrs :href)
+        alt-href  (-> alt-link :attrs :href)]
+    (is (= (headers "Content-Type") "application/atom+xml; charset=utf-8"))
+    (is (= edit-href alt-href))
+    (is (.endsWith edit-href (titleize title)))))
