0ad4bbea6bc0865b0741d142c57eb2eb5e595080
Joe Snikeris <joe@snikeris.com>
2011-08-06 01:58:36 -0400

    preparing for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject enlighten "1.0.0-SNAPSHOT"
+(defproject enlighten "0.1.0"
   :description
   "An atom-enabled blog, written in Clojure, starring Compojure and Enlive."
   :dependencies [[org.clojure/clojure "1.2.1"]
