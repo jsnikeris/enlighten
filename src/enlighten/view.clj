cb23ed65d9c1955d64e55a3860eee32a0285b76d
Joe Snikeris <joe@snikeris.com>
2011-08-10 15:13:36 -0400

    added prev, next, last links

--- src/enlighten/view.clj
+++ src/enlighten/view.clj
@@ -6,6 +6,11 @@
 (defn pprint-date [s]
   (tf/unparse (tf/formatter "MMMM d, yyyy") (tf/parse s)))
 
+(defn link-context [entry]
+  {:prev (a/permalink (m/prev-entry entry))
+   :next (a/permalink (m/next-entry entry))
+   :last (a/permalink (first (m/get-entries)))})
+
 (e/deftemplate entry "templates/entry.html" [entry]
   [#{:title :#title}] (e/content (e/select-text entry [:title]))
   [:#content] (e/content (e/select entry [:content :> :*]))
@@ -13,11 +18,11 @@
     #(let [pub-date (e/select-text entry [:published])]
        ((e/do-> (e/content (pprint-date pub-date))
                 (e/set-attr :datetime pub-date)) %))
-  [:nav :li]
-    #(when-let [href (case (e/select-attr % [:a] :rel)
-                       "prev" (a/permalink (m/prev-entry entry))
-                       "next" (a/permalink (m/next-entry entry))
-                       "last" (a/permalink (first (m/get-entries)))
-                       nil)]
-       (e/at %
-         [:a] (e/set-attr :href href))))
+  #{[:nav :li] [:link]}
+    #(let [ctxt (link-context [entry])
+           link-sel #{[:a] [:link]}
+           rel-kw (keyword (e/select-attr % link-sel :rel))]
+       (if (contains? ctxt rel-kw)
+         (when-let [href (rel-kw ctxt)]
+           (e/at % link-sel (e/set-attr :href href)))
+         %)))
