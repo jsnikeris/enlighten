8da1b80421376664d5ab2e29cbda7aa76ba41863
Joe Snikeris <joe@snikeris.com>
2011-08-13 10:39:34 -0400

    made project uberjar-able

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
                  [compojure "0.6.5"]
                  [com.snikeris/enlive "2.0.0"]
                  [ring-mock "0.1.1"]
+                 [ring "0.3.11"]
                  [clj-time "0.3.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
+  :main enlighten.core
   :ring {:handler enlighten.core/app})
